d99c1125fa62bb12850e73c1e558da33eaca5cac
JeevaM15 <161304460+JeevaM15@users.noreply.github.com>
2024-04-26 17:06:02 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$con=mysqli_connect("certidb.mysql.database.azure.com","azureuser","sangeetha7519@","cert");
+$con=mysqli_connect("certidb.mysql.database.azure.com","","sangeetha7519@","cert");
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());}
 ?>
